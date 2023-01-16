581bae689d9d73f39023dcbafd5858a9847f7b00
vincennt99v1123li <49277518+vincennt99v1123li@users.noreply.github.com>
2023-01-16 20:38:53 +0800

    Add files via upload

---
+++ cifar10/tensor_cifar10_5.py
@@ -0,0 +1,83 @@
+import datetime
+
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
+
+import yaml
+
+yamlPath = "config.yaml"
+
+config = []
+with open(yamlPath,'rb') as f:
+    config = yaml.safe_load(f)
+
+data_path = config['data_path']
+epochs = config['epochs']
+batch_size = config['batch_size']
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+
+
+'''
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i])
+    # The CIFAR labels happen to be arrays,
+    # which is why you need the extra index
+    plt.xlabel(class_names[train_labels[i][0]])
+    
+#plt.show()
+'''
+
+#test3
+
+model = models.Sequential()
+model.add(layers.Conv2D(6, (5, 5), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(16, (5, 5), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+
+model.add(layers.Flatten())
+model.add(layers.Dense(120, activation='relu'))
+model.add(layers.Dense(84, activation='relu'))
+model.add(layers.Dense(10))
+
+model.summary()
+opt = tf.keras.optimizers.SGD(learning_rate=0.001, momentum=0.9)
+model.compile(optimizer=opt,
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+
+starttime = datetime.datetime.now()
+history = model.fit(train_images, train_labels, epochs=epochs, batch_size=batch_size,
+                    validation_data=(test_images, test_labels))
+
+endtime = datetime.datetime.now()
+seconds = (endtime - starttime).seconds
+m, s = divmod(seconds, 60)
+h, m = divmod(m, 60)
+print("total training time : %02d:%02d:%02d" % (h, m, s))
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
